322c2de5560020908e52ddb48df80dd73c85cdf4
saltyfish <i237731947@outlook.com>
2020-02-01 23:11:55 +0800

    完善指令
    mov
    jbe,jae
    cmpsb

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * @Author: taowentao
  * @Date: 2020-02-01 12:05:25
  * @LastEditors  : taowentao
- * @LastEditTime : 2020-02-01 19:39:52
+ * @LastEditTime : 2020-02-01 23:11:22
  */
 
 #include <iostream>
@@ -22,12 +22,22 @@ enum TVM_OP
     MOV = MOV_RR,
 
     //mov [r0],2
-    //*r0=2
-    MOV_AI,
+    //ds[r0]=2
+    MOV_RA_I,
+    MOV_AI = MOV_RA_I,
 
     //mov [r0],r1
-    //*r0=r1
-    MOV_AR,
+    //ds[r0]=r1
+    MOV_RA_R,
+    MOV_AR = MOV_RA_R,
+
+    //mov r0,[r1]
+    //r0=ds[r1]
+    MOV_R_RA,
+
+    //mov r0,[0]
+    //r0=ds[0]
+    MOV_R_IA,
 
     //add r0,1
     //r0+=1
@@ -95,6 +105,11 @@ enum TVM_OP
     SHR_RI,
     SHR_RR,
     SHR = SHR_RR,
+
+    //比较字符串
+    //cmpsb r1,r2
+    //比较r1和r2指向的地址的r0个长度的字节是否都相等,相等zf=1,否则zf=0
+    CMPSB,
 
     //push 1
     //--sp
@@ -168,7 +183,10 @@ enum TVM_OP
     JA_R,
     JA = JA_R,
 
-    // JNA,
+    JAE_I,
+
+    JAE_R,
+    JAE = JAE_R,
 
     //jb 100
     //sf==1&&zf==0 ip+=100
@@ -176,7 +194,11 @@ enum TVM_OP
 
     JB_R,
     JB = JB_R,
-    // JNB,
+
+    JBE_I,
+
+    JBE_R,
+    JBE = JBE_R,
 
     //nop
     NOP,
@@ -244,6 +266,18 @@ public:
                 mov_ar();
                 ip += 2;
                 break;
+            case MOV_AI:
+                mov_ai();
+                ip += 2;
+                break;
+            case MOV_R_IA:
+                mov_r_ia();
+                ip += 2;
+                break;
+            case MOV_R_RA:
+                mov_r_ra();
+                ip += 2;
+                break;
             case ADD_RI:
                 add_ri();
                 ip += 2;
@@ -284,6 +318,10 @@ public:
                 cmp_rr();
                 ip += 2;
                 break;
+            case CMPSB:
+                cmpsb();
+                ip += 2;
+                break;
             case JMP_I:
                 jmp_i();
                 ip += 1;
@@ -316,12 +354,28 @@ public:
                 ja_r();
                 ip += 1;
                 break;
+            case JAE_I:
+                jae_i();
+                ip += 1;
+                break;
+            case JAE_R:
+                jae_r();
+                ip += 1;
+                break;
             case JB_I:
                 jb_i();
                 ip += 1;
                 break;
             case JB_R:
                 jb_r();
+                ip += 1;
+                break;
+            case JBE_I:
+                jbe_i();
+                ip += 1;
+                break;
+            case JBE_R:
+                jbe_r();
                 ip += 1;
                 break;
             case PUSH_I:
@@ -373,6 +427,13 @@ private:
         int b = cs[ip + 1];
         r[a] = r[b];
     }
+    void mov_ai(){
+        //第1个操作数(寄存器)
+        int a = cs[ip];
+        //第2个操作数(立即数)
+        int b = cs[ip + 1];
+        ds[r[a]] = b;
+    }
     void mov_ar()
     {
         //第1个操作数(寄存器)
@@ -380,6 +441,20 @@ private:
         //第2个操作数(寄存器)
         int b = cs[ip + 1];
         ds[r[a]] = r[b];
+    }
+    void mov_r_ra(){
+        //第1个操作数(寄存器)
+        int a = cs[ip];
+        //第2个操作数(寄存器)
+        int b = cs[ip + 1];
+        r[a] = ds[r[b]];
+    }
+    void mov_r_ia(){
+        //第1个操作数(寄存器)
+        int a = cs[ip];
+        //第2个操作数(立即数)
+        int b = cs[ip + 1];
+        r[a] = ds[b];
     }
     void add_ri()
     {
@@ -472,7 +547,7 @@ private:
     {
         //第1个操作数(寄存器)
         int a = cs[ip];
-        //第2个操作数(立即数)
+        //第2个操作数(寄存器)
         int b = cs[ip + 1];
         if (r[a] == r[b])
         {
@@ -489,6 +564,24 @@ private:
             sf = 0;
             zf = 0;
         }
+    }
+    void cmpsb(){
+        //第1个操作数(寄存器)
+        int a = cs[ip];
+        //第2个操作数(寄存器)
+        int b = cs[ip + 1];
+        const char *pca = reinterpret_cast<char *>(r[a]);
+        const char *pcb = reinterpret_cast<char *>(r[b]);
+        int len = r[0];
+        bool f = true;
+        for (int i = 0; i < len; ++i)
+        {
+            if(pca[i]!=pcb[i]){
+                f = false;
+                break;
+            }
+        }
+        zf = f ? 1 : 0;
     }
     void jmp_i(){
         //第1个操作数(立即数)
@@ -555,6 +648,24 @@ private:
             ip += r[a];
         }
     }
+    void jae_i()
+    {
+        if (zf==1 || sf == 0)
+        {
+            //第1个操作数(立即数)
+            int a = cs[ip];
+            ip += a;
+        }
+    }
+    void jae_r()
+    {
+        if (zf == 1 || sf == 0)
+        {
+            //第1个操作数(寄存器)
+            int a = cs[ip];
+            ip += r[a];
+        }
+    }
     void jb_i()
     {
         if (sf == 1 && zf == 0)
@@ -567,6 +678,24 @@ private:
     void jb_r()
     {
         if (sf == 1 && zf == 0)
+        {
+            //第1个操作数(寄存器)
+            int a = cs[ip];
+            ip += r[a];
+        }
+    }
+    void jbe_i()
+    {
+        if (zf == 1 || sf == 1)
+        {
+            //第1个操作数(立即数)
+            int a = cs[ip];
+            ip += a;
+        }
+    }
+    void jbe_r()
+    {
+        if (zf == 1 || sf == 1)
         {
             //第1个操作数(寄存器)
             int a = cs[ip];
@@ -614,12 +743,20 @@ private:
 };
 } // namespace TVM
 
-int main(int argc, char const *argv[])
-{
+#include <string>
+
+using namespace std;
+
+void test_vm(){
     using namespace TVM;
-    using namespace std;
     int a = 101, b = 203;
-    int TVM_DATA[100] = {0};
+    char stra[] = "hello1";
+    char strb[] = "hello2";
+    char *pa = stra;
+    char *pb = strb;
+    int pstra = *(int *)&pa;
+    int pstrb = *(int *)&pb;
+    int TVM_DATA[100] = {0, 11, 22};
     int TVM_CODE[] = {
         //子函数add
         POP,R3,//保存IP
@@ -637,19 +774,90 @@ int main(int argc, char const *argv[])
         MOV_RI,R1,0,//r1=0
         MOV_AR,R1,R0,//ds[r1]=r0
 
-        //测试jmp等
-        MOV_RI,R0,0,//r0=0
-        MOV_RI,R1,1,//r1=0
-        MOV_RI,R2,3,//r2=3
-        CMP_RR,R0,R1,//r0 ? r1
-        JB_R,R2,
-        ADD_RI,R0,1,
-        ADD_RI,R0,1,//是否会跳到这
-        ADD_RI,R0,1,
+        //测试mov
+        MOV_R_IA,R1,1,
+        MOV_RI,R0,2,
+        MOV_R_RA,R2,R0,
+
+        //测试cmpsb
+        MOV_RI,R1,pstra,
+        MOV_RI,R2,pstrb,
+        MOV_RI,R0,sizeof(stra)-1-1,
+        CMPSB,R1,R2,
+
+        // //测试jmp等
+        // MOV_RI,R0,0,//r0=0
+        // MOV_RI,R1,1,//r1=1
+        // MOV_RI,R2,3,//r2=3
+        // CMP_RR,R0,R1,//r0 ? r1
+        // JAE_R,R2,
+        // ADD_RI,R0,1,
+        // ADD_RI,R0,1,//是否会跳到这
+        // ADD_RI,R0,1,
         END,
     };
     TCPU tc(TVM_CODE+12,TVM_DATA);
     tc.Run();
     cout<<"a+b=" << TVM_DATA[0] << endl;
+}
+
+bool test_vm_pwd(const string& name,const string&pwd){
+    //密码=用户名
+    using namespace TVM;
+    bool r = true;
+    const char *cname = name.c_str();
+    int pname = *(int *)&cname;
+    const char *cpwd = pwd.c_str();
+    int ppwd = *(int *)&pwd;
+    int TVM_DATA[100] = {0};
+    // //原始代码
+    // if (name.length() != pwd.length()){
+    //     TVM_DATA[0] = 0;
+    // }else{
+    //     if (memcmp(cname, cpwd, pwd.c_str())==0)
+    //     {
+    //         TVM_DATA[0] = 1;
+    //     }
+    // }
+    int TVM_CODE[] = {
+        //mov r0,name.length()
+        MOV_RI,R0,(int)name.length(),
+        //mov r1,pwd.length()
+        MOV_RI,R1,(int)pwd.length(),
+        //cmp r0,r1
+        CMP,R0,R1,
+        //je 1
+        JE_I,1,
+        //END
+        END,
+        //mov r1,ds[cname-ds] //r1=*(int*)cname
+        MOV_RI,R1,pname,
+        //mov r2,ds[cpwd-ds] //r2=*(int*)cname
+        MOV_RI,R2,ppwd,
+        //mov r0,pwd.length()
+        MOV_RI,R0,(int)pwd.length(),
+        //cmpsb r1,r2
+        CMPSB,R1,R2,
+        //jne 6 //不相等就跳过赋值1
+        JNE_I,6,
+        //mov r0,0
+        MOV_RI,R0,0,
+        //mov [r0],1
+        MOV_RA_I,R0,1,
+        END,
+    };
+    TCPU tc(TVM_CODE, TVM_DATA);
+    tc.Run();
+    r = TVM_DATA[0] == 1;
+    return r;
+}
+
+int main(int argc, char const *argv[])
+{
+    // test_vm();
+    bool f=test_vm_pwd("123","234");
+    cout << f << endl;
+    f = test_vm_pwd("123", "123");
+    cout << f << endl;
     return 0;
 }
